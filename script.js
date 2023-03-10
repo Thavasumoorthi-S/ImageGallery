7e034bf6b84c9eaf7bb356aaf0b2abcd9b48287d
Thavasumoorthi-S <123466522+Thavasumoorthi-S@users.noreply.github.com>
2023-03-10 23:16:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function opendisplay(img,alt) {
-	var modal = document.getElementById("myModal");
-	var modalImg = document.getElementById("img01");
+	let modal = document.getElementById("myModal");
+	let modalImg = document.getElementById("img01");
 	document.getElementById('bottom').innerHTML=alt;
 	modal.style.display = "block";
 	modalImg.src = img;
 }
 
 function closedisplay() {
-	var modal = document.getElementById("myModal");
+	let modal = document.getElementById("myModal");
 	modal.style.display = "none";
 }
 
